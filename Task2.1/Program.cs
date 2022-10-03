927f228ec003ed563a85c0eaab67fd7ea4e1933e
Anastasya <okayota83@mail.ru>
2022-10-03 17:09:20 +0400

    add Task2.1

---
+++ Task2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+if (n>0)
+    if (n<=5)
+    {
+    Console.Write("Рабочий день");
+    }
+else if (n==6)
+
+{
+   Console.Write("Выходной"); 
+}
+
+else if (n==7)
+
+{
+   Console.Write("Выходной день"); 
+}
+
+else Console.Write("Это не день недели");
